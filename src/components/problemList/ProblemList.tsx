48b67897449978f2e04362a12c6260ce6880283e
rita <413235793@qq.com>
2018-10-25 20:00:20 +0800

    try

---
+++ src/components/problemList/ProblemList.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+// import { connect } from 'react-redux';
+// import { selectPath } from '$selectors/route';
+
+const { Component } = React;
+
+class ProblemList extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <p>从没见过如此厚颜无耻之徒</p>
+      </div>
+    );
+  }
+
+}
+
+export default ProblemList;
+  
