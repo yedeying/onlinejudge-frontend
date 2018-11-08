c5240088be37b63faf07d3de6a78838c991a16cb
yedeying <yedeying999@gmail.com>
2018-11-08 21:29:50 +0800

    refactor(redux): fix lint style

--- src/redux/utils.ts
+++ src/redux/utils.ts
@@ -9,16 +9,18 @@ export const record = <T extends AnyObject>(plain: T): Record<T> => {
   return Map(plain) as any;
 };
 
-export const handleActions = <State>(
+export const handleActions = function<State>(
   handlers: ReducerMap<State>,
   defaultState: State
-) => (state: State | undefined, action: Action) => {
-  state = state || defaultState;
-  const { type } = action;
-  if (handlers[type]) {
-    return handlers[type](state, action);
-  }
-  return state;
+) {
+  return (state: State | undefined, action: Action) => {
+    state = state || defaultState;
+    const { type } = action;
+    if (handlers[type]) {
+      return handlers[type](state, action);
+    }
+    return state;
+  };
 };
 
 export const combineReducers = aliasCombineReducers;
