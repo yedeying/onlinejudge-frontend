b7067bd8078b28c0491c1d116c4fcf97b259883e
yedeying <yedeying999@gmail.com>
2018-11-09 09:55:19 +0800

    refactor(rebase): rebase root to current

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,3 +1,4 @@
+// 路由
 export enum Path {
   ROOT = '/',
   TRAINING = '/training',
@@ -6,6 +7,7 @@ export enum Path {
   LOGIN = '/login',
   REGISTER = '/register',
   TRAINING_PROBLEMS = '/training/problems',
+  TRAINING_PROBLEMS_NO = '/training/problems/:no',
   TRAINING_STATUS = '/training/status',
   TRAINING_RANKLIST = '/training/ranklist',
   CONTESTS_PROBLEMS = '/contests/:id/problems',
@@ -14,6 +16,7 @@ export enum Path {
   CONTESTS_STATISTICS = '/contests/:id/statistics'
 }
 
+// 请求
 export enum URL {
   ROOT = '/',
   TRAINING = '/training',
